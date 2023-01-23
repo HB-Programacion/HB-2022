0b2b4ace037b1ed4c27e73146010dfce9c27d992
NardaC <nardac6@gmail.com>
2023-01-23 14:10:48 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import HeinekenFest from "./components/HeinekenFest/HeinekenFest";
-import { ParallaxProvider } from 'react-scroll-parallax';
-
+import { ParallaxProvider } from "react-scroll-parallax";
 
 import "./App.css";
 
 function App() {
   return (
-    <ParallaxProvider>
-    <Header/>
+    <div className="main">
+      <Header />
       <Router>
         <Routes>
-          <Route  exact path="/" element={<Home/>}/>
-          <Route  exact path="/heinekenFest" element={<HeinekenFest/>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/heinekenFest" element={<HeinekenFest />} />
         </Routes>
       </Router>
-    <Footer/>
-    </ParallaxProvider>
+      <Footer />
+    </div>
   );
 }
 
