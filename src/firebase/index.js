0eb301e57611b731e3784ad97ae46de4e5f46f03
Napster <rome.1205h@gmail.com>
2023-06-06 18:43:12 -0500

    validacion modales

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,8 @@ import { setDoc, doc } from "firebase/firestore"
 import { db } from "./firebase.config"
 
 export const uploadDocToFirebase = async (id, data) => {
+   console.log('id', id)
+   console.log('data', data)
    if( id && data ){
       try{
          const docRef = await setDoc(doc(db, "userContact", id), data);
