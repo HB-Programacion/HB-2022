b220e73eac8a975ef6678e263257bea82769d322
NardaC <nardac6@gmail.com>
2023-11-03 16:35:34 -0500

    about img

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,7 +19,7 @@ import arrowdown from "../../assets/about/arrowdown.svg";
 
 import "./About.css";
 import MarqueeHB from "../MarqueeHB/MarqueeHB";
-import SlideMeetMarkers from "../slideMeetMakers/slideMeetMakers"
+import SlideMeetMarkers from "../slideMeetMakers/slideMeetMakers";
 
 import people1 from "../../assets/about/people/Team_1.gif";
 import people2 from "../../assets/about/people/Team_2.gif";
@@ -38,19 +38,30 @@ const About = () => {
 
   const tabs = [
     {
-      title: 'BEYOND',
-      subtitle: 'BEYOND A SINGLE DISCIPLINE',
+      title: "BEYOND",
+      subtitle: "BEYOND A SINGLE DISCIPLINE",
       description: (
         <div>
-          <p>At the heart of our process lies an understanding that the world doesn't fit into neat boxes, and neither do brands. We’re not bound by traditional lanes of design, strategy, or creativity.</p>
-          <p>Instead, we fuse them, creating an amalgam that resonates with the diverse, dynamic nature of today's audiences. </p>
-          <p>Our multidisciplinary approach ensures that every project is viewed from multiple angles, offering holistic solutions that stand out and stay relevant.</p>
+          <p>
+            At the heart of our process lies an understanding that the world
+            doesn't fit into neat boxes, and neither do brands. We’re not bound
+            by traditional lanes of design, strategy, or creativity.
+          </p>
+          <p>
+            Instead, we fuse them, creating an amalgam that resonates with the
+            diverse, dynamic nature of today's audiences.{" "}
+          </p>
+          <p>
+            Our multidisciplinary approach ensures that every project is viewed
+            from multiple angles, offering holistic solutions that stand out and
+            stay relevant.
+          </p>
         </div>
       ),
     },
     {
-      title: 'SHARED',
-      subtitle: 'SHARED A SINGLE DISCIPLINE',
+      title: "SHARED",
+      subtitle: "SHARED A SINGLE DISCIPLINE",
       description: (
         <div>
           <p>Descripción de la pestaña 1.</p>
@@ -60,8 +71,8 @@ const About = () => {
       ),
     },
     {
-      title: 'DEEPER',
-      subtitle: 'DEEPER A SINGLE DISCIPLINE',
+      title: "DEEPER",
+      subtitle: "DEEPER A SINGLE DISCIPLINE",
       description: (
         <div>
           <p>Descripción de la pestaña 1.</p>
@@ -78,7 +89,7 @@ const About = () => {
 
   const AccordionItem = ({ title, content }) => {
     const [isOpen, setIsOpen] = useState(false);
-  
+
     const toggleAccordion = () => {
       setIsOpen(!isOpen);
     };
@@ -86,23 +97,23 @@ const About = () => {
     return (
       <div className="accordion-item">
         <div className="accordion-item_content">
-          <button
-            className="accordion-button"
-            onClick={toggleAccordion}>
-            <div>
-              {title}
-            </div>
-            {isOpen ?
-              <img src={arrowDown} alt="arrowDown" style={{transform: 'rotate(180deg)'}} />
-              :
-              <img src={arrowDown} alt="arrowDown"/>
-            }
+          <button className="accordion-button" onClick={toggleAccordion}>
+            <div>{title}</div>
+            {isOpen ? (
+              <img
+                src={arrowDown}
+                alt="arrowDown"
+                style={{ transform: "rotate(180deg)" }}
+              />
+            ) : (
+              <img src={arrowDown} alt="arrowDown" />
+            )}
           </button>
         </div>
         {isOpen && <div className="accordion-content">{content}</div>}
       </div>
     );
-  }
+  };
 
   return (
     <>
@@ -112,30 +123,64 @@ const About = () => {
           <img src={meet} className="img-tiwwa" />
           <h3 className="title-about">Design, Develop, Deliver</h3>
           <h6 className="subtitle-about">
-            Our actions aren’t just words. They are the backbone of what we do. Every idea, every concept, begins with a spark and we are passionate about turning that spark into tangible creations. From the earliest sketch of a brand identity to the final execution of a marketing campaign, we believe in prototyping, refining, and perfecting. Every product, every project, is a testament to our commitment to excellence and innovation.
+            Our actions aren’t just words. They are the backbone of what we do.
+            Every idea, every concept, begins with a spark and we are passionate
+            about turning that spark into tangible creations. From the earliest
+            sketch of a brand identity to the final execution of a marketing
+            campaign, we believe in prototyping, refining, and perfecting. Every
+            product, every project, is a testament to our commitment to
+            excellence and innovation.
           </h6>
           <h3 className="title-about">Mixed Mentality</h3>
           <h6 className="subtitle-about">
-            An integrated, interdisciplinary approach lies at the heart of our philosophy. Recognizing that the most compelling brands are built from their core offerings, we champion a foundation that fosters a distinct identity and potent marketing. Beyond mere external perceptions, it's a matter of orchestrating an ecosystem where product, brand, and communication harmonize effortlessly. In this fusion, brands are poised to resonate profoundly, accelerate growth, and command attention.
+            An integrated, interdisciplinary approach lies at the heart of our
+            philosophy. Recognizing that the most compelling brands are built
+            from their core offerings, we champion a foundation that fosters a
+            distinct identity and potent marketing. Beyond mere external
+            perceptions, it's a matter of orchestrating an ecosystem where
+            product, brand, and communication harmonize effortlessly. In this
+            fusion, brands are poised to resonate profoundly, accelerate growth,
+            and command attention.
           </h6>
         </div>
         <img src={img1} className="w-100 img1" />
-        <MarqueeHB/>
+        <MarqueeHB />
         <div className="box-about">
           <h3 className="title-about text-mutual">Mutual Trust Manifesto</h3>
           <h6 className="subtitle-about">
-            In a world increasingly driven by technology, we anchor ourselves in genuine human connection. Here, it's not just about the job, it's about the bond we share as a team of dedicated professionals. These are the cornerstones that guide our work:
+            In a world increasingly driven by technology, we anchor ourselves in
+            genuine human connection. Here, it's not just about the job, it's
+            about the bond we share as a team of dedicated professionals. These
+            are the cornerstones that guide our work:
             <AccordionItem
               title="Unified Vision"
-              content={<p>Each individual offers a unique perspective. Melded together, these insights allow us to craft holistic, impactful solutions.</p>}
+              content={
+                <p>
+                  Each individual offers a unique perspective. Melded together,
+                  these insights allow us to craft holistic, impactful
+                  solutions.
+                </p>
+              }
             />
             <AccordionItem
               title="Endless Curiosity"
-              content={<p>Our commitment to constant learning and innovation shapes the core of who we are. Diverse backgrounds and expertise feed our ceaseless quest for the next big idea.</p>}
+              content={
+                <p>
+                  Our commitment to constant learning and innovation shapes the
+                  core of who we are. Diverse backgrounds and expertise feed our
+                  ceaseless quest for the next big idea.
+                </p>
+              }
             />
             <AccordionItem
               title="Collaborative Spirit"
-              content={<p>Every achievement is the result of the harmony and synergy that resonates within our team. Mutual respect, understanding, and collective growth underscore our every interaction.</p>}
+              content={
+                <p>
+                  Every achievement is the result of the harmony and synergy
+                  that resonates within our team. Mutual respect, understanding,
+                  and collective growth underscore our every interaction.
+                </p>
+              }
             />
           </h6>
           <img src={img2} className="w-100 img1" />
@@ -143,7 +188,9 @@ const About = () => {
         <div className="box-about ">
           <img src={makers} className="mop" />
           <p className="text-mop">
-            Discover the heartbeat of our studio. Together, we challenge conventions, celebrate diversity, and drive innovation. Unfold the layers of our team and witness the collaboration that defines us.
+            Discover the heartbeat of our studio. Together, we challenge
+            conventions, celebrate diversity, and drive innovation. Unfold the
+            layers of our team and witness the collaboration that defines us.
           </p>
         </div>
         <div className="box-about">
@@ -190,34 +237,45 @@ const About = () => {
             </div>
           </div> */}
 
-<SlideMeetMarkers/>
+          <SlideMeetMarkers />
         </div>
         <div className="box-about">
           <img src={img3} className="w-100 img1" />
         </div>
       </div>
       <div className="bg-white">
-        <img src={img4} className="w-100 img1" />
+        <div className="box-about">
+          {" "}
+          <p className="p-black text-acreative">
+            A creative agency where strategy, design, and creativity converge.
+            Over the years, we've honed our belief that a standout brand emerges
+            when these three elements seamlessly intertwine.
+          </p>
+          <img src={img4} className="w-100 img1" />
+        </div>
         <div className="box-about">
           {/* <p className="p-black">
             A creative agency where strategy, design, and creativity converge. Over the years, we've honed our belief that a standout brand emerges when these three elements seamlessly intertwine.
           </p> */}
 
           <img src={intrinsec} className="wwb" />
-          <p className="wwb-textdown">Henri Barrett is more than just an agency; it's a movement, a mindset. We're here to change narratives, elevate brands, and make a lasting imprint in the world of design and creativity. Dive into our guiding principles, the essence of what makes us Henri Barrett.</p>
+          <p className="wwb-textdown">
+            Henri Barrett is more than just an agency; it's a movement, a
+            mindset. We're here to change narratives, elevate brands, and make a
+            lasting imprint in the world of design and creativity. Dive into our
+            guiding principles, the essence of what makes us Henri Barrett.
+          </p>
           <div className="box-wwb-numbers">
             <div className="box-wwb-tabs">
-              {
-                tabs.map((tab, index) => (
-                  <div
-                    key={index}
-                    className={`tab ${activeTab === index ? 'active' : ''}`}
-                    onClick={() => handleTabClick(index)}
-                  >
-                    {tab.title}
-                  </div>
-                ))
-              }
+              {tabs.map((tab, index) => (
+                <div
+                  key={index}
+                  className={`tab ${activeTab === index ? "active" : ""}`}
+                  onClick={() => handleTabClick(index)}
+                >
+                  {tab.title}
+                </div>
+              ))}
             </div>
             <div className="tab-content">
               <h2 className="title-tab">{tabs[activeTab].subtitle}</h2>
@@ -229,7 +287,10 @@ const About = () => {
       <div className="bg-black fondo-surprise">
         <div className="box-title-surprise">
           <img src={random} className="img-surprise-kit" />
-          <h1 className="text-random">Take a break, download and explore this tad. Who knows where our bits of randomness will lead you?</h1>
+          <h1 className="text-random">
+            Take a break, download and explore this tad. Who knows where our
+            bits of randomness will lead you?
+          </h1>
           <button className="btn-white-surprise mx2-auto">
             download <img src={arrowdown} />
           </button>
