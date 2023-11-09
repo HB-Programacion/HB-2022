b0f3ee520b6f2d78a208c197574077f17d092980
Napster <rome.1205h@gmail.com>
2023-11-09 11:50:54 -0500

    update videos makers

---
+++ src/components/data/dataMeetMakers.js
@@ -0,0 +1,115 @@
+import peopleV1 from '../../assets/about/people/RIC AJU.mp4'
+import people2 from "../../assets/about/people/foto2.png";
+import people3 from "../../assets/about/people/foto3.png";
+import people4 from "../../assets/about/people/foto4.png";
+import people5 from "../../assets/about/people/foto5.png";
+import people6 from "../../assets/about/people/foto6.png";
+
+import peopleV2 from "../../assets/about/people/Tate_Res.mp4";
+import peopleV3 from "../../assets/about/people/ADRI AJUS.mp4";
+import peopleV4 from "../../assets/about/people/CARLITA AJU.mp4";
+import peopleV5 from "../../assets/about/people/GIAN AJU.mp4";
+import peopleV6 from "../../assets/about/people/Guille_Aju.mp4";
+import peopleV7 from "../../assets/about/people/LUCHO AJU.mp4";
+import peopleV8 from "../../assets/about/people/MORE AJU.mp4";
+import peopleV9 from "../../assets/about/people/OS AJU.mp4";
+import peopleV10 from "../../assets/about/people/JOSE AJU.mp4";
+
+export const meetMakers = [
+  {
+    className: "img-l",
+    source: peopleV1,
+    title: "Maria José Ramirez 1",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV2,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV3,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV4,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV5,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV6,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV7,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV8,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV9,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV10,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV1,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV2,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV3,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV4,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  },
+  {
+    className: "img-l",
+    source: peopleV3,
+    title: "Maria José Ramirez 3",
+    description: "Production",
+  },
+  {
+    className: "img-s",
+    source: peopleV4,
+    title: "Maria José Ramirez 2",
+    description: "Production",
+  }
+];
