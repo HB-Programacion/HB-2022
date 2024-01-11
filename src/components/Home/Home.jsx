a9c5375120503a59f32c47915f9e205212027fcb
Napster <rome.1205h@gmail.com>
2024-01-11 18:24:10 -0500

    integration aws

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import img2 from "../../assets/home/img-2.png";
-import img2D from "../../assets/home/desktop/img-our-services-desktop.png";
-import workParallax from "../../assets/home/work-parallax.svg";
+import img2 from "https://webhb.s3.us-east-2.amazonaws.com/Home/img-2.png";
+import img2D from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/img-our-services-desktop.png";
+import workParallax from "https://webhb.s3.us-east-2.amazonaws.com/Home/work-parallax.svg";
 
 import arrowbtn from "../../assets/arrow-btn.svg";
-import union from "../../assets/home/union.svg";
-import groupwwd from "../../assets/home/img-proyectos.png";
-import groupwwdD from "../../assets/home/desktop/group-services-desktop.png";
+import union from "https://webhb.s3.us-east-2.amazonaws.com/Home/union.svg";
+import groupwwd from "https://webhb.s3.us-east-2.amazonaws.com/Home/img-proyectos.png";
+import groupwwdD from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/group-services-desktop.png";
 
-import hnk from "../../assets/home/hnk-home-mobile.png";
-import umana from "../../assets/home/umana-home-mobile.png";
-import teclab from "../../assets/home/teclab-home-mobile.png";
-import rappi from "../../assets/home/rappi-home-mobile.png";
-import rappiD from "../../assets/home/desktop/rappi-home-desktop.png";
+import hnk from "https://webhb.s3.us-east-2.amazonaws.com/Home/hnk-home-mobile.png";
+import umana from "https://webhb.s3.us-east-2.amazonaws.com/Home/umana-home-mobile.png";
+import teclab from "https://webhb.s3.us-east-2.amazonaws.com/Home/teclab-home-mobile.png";
+import rappi from "https://webhb.s3.us-east-2.amazonaws.com/Home/rappi-home-mobile.png";
+import rappiD from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/rappi-home-desktop.png";
 
-import hnkD from "../../assets/home/desktop/hnk-desktop.png";
-import umanaD from "../../assets/home/desktop/umana-desktop.png";
-import teclabD from "../../assets/home/desktop/teclab-desktop.png";
+import hnkD from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/hnk-desktop.png";
+import umanaD from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/umana-desktop.png";
+import teclabD from "https://webhb.s3.us-east-2.amazonaws.com/Home/desktop/teclab-desktop.png";
 
 import title from "../../assets/contact/tiltle-contact-home.svg";
 import titleD from "../../assets/contact/title-contact-desktop.png";
